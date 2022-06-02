5eab8a11b8c6f771878797b108abdac0b0409ac9
DilithiumNitrate <38111406+DilithiumNitrate@users.noreply.github.com>
2022-06-03 01:06:02 +0200

    Implement dice rolling
    Unrelated to Wordle, but we promised that I guess

---
+++ src/commands/misc.rs
@@ -0,0 +1,43 @@
+use crate::{Context, Error};
+use rand::prelude::*;
+use poise::serenity_prelude as serenity;
+
+/// Roll dice
+/// 
+/// Allows specifying die size and die count.
+#[poise::command(slash_command, rename = "roll", category = "Miscellaneous")]
+pub async fn roll_dice(
+    ctx: Context<'_>,
+    #[description = "Die size"] die_size: Option<u32>,
+    #[description = "Die count"] die_count: Option<u16>,
+) -> Result<(), Error> {
+    let die_size = die_size.unwrap_or(6);
+    if die_size <= 1 {
+        ctx.say(format!("**Error:** {} is not a valid size of dice.", die_size)).await?;
+        return Ok(());
+    }
+    let die_count = die_count.unwrap_or(1);
+    ctx.say(format!("Rolling {}d{}...", die_count, die_size)).await?;
+
+    let roll: u64 = {
+        /*let mut rng = ctx.data()
+            .common_rng
+            .lock()
+            .await;*/
+        let mut rng = thread_rng();
+        let uniform = rand::distributions::Uniform::new_inclusive(1, die_size);
+        (0..die_count)
+            .map(|_| uniform.sample(&mut rng) as u64) // is there a better way?
+            .sum()
+    };
+
+    let mut output = serenity::MessageBuilder::new();
+    output.push("Result: ")
+        .push_mono(roll);
+    if die_count == 1 && die_size == 20 && roll == 20 {
+        output.push("\nDon't let it go to your head.");
+    }
+    ctx.say(output.build()).await?;
+    
+    Ok(())
+}
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,2 +1,3 @@
 pub mod dict;
 pub mod mpgame;
+pub mod misc;
